561519bf1b0f08a87b8b39899bc9b2eb1d96ca85
Matias Oñate <mationate.0@gmail.com>
2019-03-05 10:51:27 -0300

    ADD DEV: active_admin functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :proyects
   devise_for :municipalities
   devise_for :users
