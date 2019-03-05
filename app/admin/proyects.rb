fc8424fc633a015d942701fcb986100d50838f04
Matias Oñate <mationate.0@gmail.com>
2019-03-05 11:12:16 -0300

    panels on activeadmin with filters and params

--- app/admin/proyects.rb
+++ app/admin/proyects.rb
@@ -2,9 +2,23 @@ ActiveAdmin.register Proyect do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
 #
-# or
+permit_params :title, :description, :budget, :address, :lattitude, :longitude, :start_date, :finish_date
+
+form do |f|
+  inputs 'Crear un proyecto' do
+    input :title
+    input :description
+    input :budget
+    input :address
+    input :lattitude
+    input :longitude
+    input :start_date
+    input :finish_date
+
+  end
+  actions
+end
 #
 # permit_params do
 #   permitted = [:permitted, :attributes]
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,8 +1,10 @@
-ActiveAdmin.register Municipality do
+ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
 permit_params :email, :password
 filter :email, as: :select
 #
@@ -22,9 +24,6 @@ form do |f|
   end
   actions
 end
-
-# or
-#
 # permit_params do
 #   permitted = [:permitted, :attributes]
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
